47ae688d20301e9cc9f5045493dd508c93b2dc7d
‘'陈飞岳 <chenfy@citic.com>
2018-12-21 17:00:21 +0800

    '1.0.0.1'

--- web-api/src/main/java/com/cfygit/webapi/WebApiApplication.java
+++ web-api/src/main/java/com/cfygit/webapi/WebApiApplication.java
@@ -3,9 +3,11 @@ package com.cfygit.webapi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.cfygit.webapi.mapper")
+@EnableDiscoveryClient
 public class WebApiApplication {
 
 	public static void main(String[] args) {
